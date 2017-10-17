3c56ef73b75d0e7f5aa343de8b2bebfd4269805c
root <john.yu@reisystems.com>
2017-10-17 17:54:58 +0000

    catching errors started

--- harvester_check.py
+++ harvester_check.py
@@ -8,14 +8,13 @@ from email import Utils
 
 file_name = '/var/log/harvester_run.log'
 current_time = datetime.now()
-_ERRORS = False
-recipient_emails = ['john.yu@reisystems.com', 'john123yu@gmail.com']
+recipient_emails = ['root@localhost']
 mail_from = 'no-reply@data.gov'
-message_body = "Hello World"
+harvester_errors = {'sqlalchemy.exc.OperationalError', ''}
 
 harvester_run_log = open(file_name, 'r').readlines()
 harvester_run_log_tail = harvester_run_log[-30:-1]
-harvester_run_log_last_line = harvester_run_log[-2:-1][0]
+#harvester_run_log_last_line = harvester_run_log[-2:-1][0]
 
 def _send_mail(mail_from='', recipient_emails=[''],
         msg=MIMEText(''.encode('utf-8'), 'plain', 'utf-8')):
@@ -35,30 +34,26 @@ def _send_mail(mail_from='', recipient_emails=[''],
         print "ERROR"
     finally:
         smtp_connection.quit()
-#for line in harvester_run_log_tail:
-#    print line
+
+for line in harvester_run_log_tail:
+    print line
+    if any(error in line for error in harvester_errors):
+        message_body = line
+
 #print harvester_run_log_last_line
-
-#print "CURRENT TIME"
-#print current_time
-
 try:
     mtime = os.path.getmtime(file_name)
 except OSError:
     mtime = 0
 last_modified = datetime.fromtimestamp(mtime)
-
-#print "file last modified"
-#print last_modified
-
-print "Time difference"
 time_difference_hours = (current_time - last_modified).total_seconds() / 3600
-print time_difference_hours
+print "Time difference: " + str(time_difference_hours)
 
-if time_difference_hours > .00001:
-    print "MORE"
-    _ERRORS = True
+if time_difference_hours > 6:
+    message_body = 'Harvest log has not been updated in 6 hours.'
 
-if _ERRORS:
-    msg = MIMEText(message_body.encode('utf-8'), 'plain', 'utf-8')
+if message_body:
+    msg = MIMEText(message_body)
+    msg['Subject'] = 'Harvest log error'
+    #msg = MIMEText(message_body.encode('utf-8'), 'plain', 'utf-8')
     _send_mail(mail_from, recipient_emails, msg)
