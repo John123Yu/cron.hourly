bfea7fff5b58f416552abc3e9bfeaee5e1114992
root <john.yu@reisystems.com>
2017-10-19 19:43:37 +0000

    tracking gather and fetch logs instead

--- harvester_check.py
+++ harvester_check.py
@@ -3,17 +3,22 @@ import time
 import smtplib
 from datetime import datetime
 from email.mime.text import MIMEText
+from subprocess import call
 
-file_name = '/var/log/harvester_run.log'
 current_time = datetime.now()
-recipient_emails = ['root@localhost']
+file_names = ('/var/log/gather-consumer.log', '/var/log/fetch-consumer.log')
 mail_from = 'no-reply@data.gov'
-harvester_errors = {'sqlalchemy.exc.OperationalError', 'Problems were found while connecting to the SOLR server','redis.exceptions.ConnectionError'}
-message_body = None
+recipient_emails = ['root@localhost']
+harvester_errors = set(['sqlalchemy.exc.OperationalError', 'Problems were found while connecting to the SOLR server','redis.exceptions.ConnectionError', 'Gather stage failed', 'Fetch stage failed'])
+message_body = ""
+all_logs = list()
 
-harvester_run_log = open(file_name, 'r').readlines()
-harvester_run_log_tail = harvester_run_log[-15:-1]
-#harvester_run_log_last_line = harvester_run_log[-2:-1][0]
+for file_name in file_names:
+    log = open(file_name, 'r').readlines()
+    log_tail = log[-15:]
+    all_logs.append(log_tail)
+
+gather_log, fetch_log = all_logs
 
 def _send_mail(mail_from='', recipient_emails=[''],
         msg=MIMEText(''.encode('utf-8'), 'plain', 'utf-8')):
@@ -29,26 +34,31 @@ def _send_mail(mail_from='', recipient_emails=[''],
         msg = '%r' % e
         print (msg)
         raise MailerException(msg)
-    except:
-        print "ERROR"
     finally:
         smtp_connection.quit()
 
-for line in harvester_run_log_tail:
-    if any(error in line for error in harvester_errors):
-        message_body = line
-        print line
+for i,log in enumerate(all_logs):
+    for line in log:
+        if any(error in line for error in harvester_errors):
+            message_body += "Gather log error: \n" if i == 0 else "Fetch log error: \n"
+            message_body += line + "\n"
 
-try:
-    mtime = os.path.getmtime(file_name)
-except OSError:
-    mtime = 0
-last_modified = datetime.fromtimestamp(mtime)
-time_difference_hours = (current_time - last_modified).total_seconds() / 3600
-#print "Time difference: " + str(time_difference_hours)
+for i,file_name in enumerate(file_names):
+    try:
+        mtime = os.path.getmtime(file_name)
+    except OSError:
+        mtime = 0
 
-if time_difference_hours > 6:
-    message_body = 'Harvest log has not been updated in 6 hours.'
+    last_modified = datetime.fromtimestamp(mtime)
+    time_difference_hours = (current_time - last_modified).total_seconds() / 3600
+
+    if time_difference_hours > 12:
+        message_body += "{0} has not been updated in 12 hours. Last time it was updated: {1} \n".format(file_name, str(last_modified))
+        if i == 0:
+            return_code = call("supervisorctl restart harvest-gather", shell=True) 
+        else:
+            return_code = call("supervisorctl restart harvest-fetch:*", shell=True)
+        print return_code
 
 if message_body:
     msg = MIMEText(message_body)
