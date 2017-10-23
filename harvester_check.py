ffaedad62f782cbef924b1c941f3e5d723cdbfd7
root <john.yu@reisystems.com>
2017-10-23 17:44:04 +0000

    shebang added, harvest_check.log added

--- harvester_check.py
+++ harvester_check.py
@@ -1,27 +1,23 @@
+#!/usr/bin/env python
 import os
 import time
 import smtplib
 from datetime import datetime
 from email.mime.text import MIMEText
 from subprocess import call
+import socket
+print(socket.gethostname())
 
 file_names = ('/var/log/gather-consumer.log', '/var/log/fetch-consumer.log')
 mail_from = 'no-reply@data.gov'
-recipient_emails = ['root@localhost', 'data.gov.support@reisystems.com']
+recipient_emails = ['data.gov.support@reisystems.com']
 harvester_errors = set(['sqlalchemy.exc.OperationalError', 'Problems were found while connecting to the SOLR server','redis.exceptions.ConnectionError', 'Gather stage failed', 'Fetch stage failed'])
+harvester_check_log = open("/var/log/harvester_check.log", "a")
 #harvester_successes = set(['objects to the fetch queue',])
 #Gather queue consumer registered
 #Fetch queue consumer registered
-
 message_body = ""
 all_logs = list()
-
-for file_name in file_names:
-    log = open(file_name, 'r').readlines()
-    log_tail = log[-15:]
-    all_logs.append(log_tail)
-
-#gather_log, fetch_log = all_logs
 
 def _send_mail(mail_from='', recipient_emails=[''],
         msg=MIMEText(''.encode('utf-8'), 'plain', 'utf-8')):
@@ -40,11 +36,19 @@ def _send_mail(mail_from='', recipient_emails=[''],
     finally:
         smtp_connection.quit()
 
+for file_name in file_names:
+    log = open(file_name, 'r').readlines()
+    log_tail = log[-15:]
+    all_logs.append(log_tail)
+
+#gather_log, fetch_log = all_logs
+
 for i,log in enumerate(all_logs):
     for line in log:
+        current_time = datetime.now()
         if any(error in line for error in harvester_errors):
-            message_body += "Gather log error: \n" if i == 0 else "Fetch log error: \n"
-            message_body += line + "\n"
+            message_body += str(current_time) + " - Gather log error: \n" if i == 0 else "Fetch log error: \n"
+            message_body += str(current_time) + " - " +  line + "\n"
 
 for i,file_name in enumerate(file_names):
     try:
@@ -55,7 +59,7 @@ for i,file_name in enumerate(file_names):
     last_modified = datetime.fromtimestamp(mtime)
     time_difference_hours = (current_time - last_modified).total_seconds() / 3600
     if time_difference_hours > 12:
-        message_body += "{0} has not been updated in 12 hours. Last time it was updated: {1}. Will perform a manual supervisor restart. \n".format(file_name, str(last_modified))
+        message_body += str(current_time) + " - {0} has not been updated in 12 hours. Last time it was updated: {1}. Will perform a manual supervisor restart. \n".format(file_name, str(last_modified))
         if i == 0:
             return_code = call("supervisorctl restart harvest-gather", shell=True) 
         elif i == 1:
@@ -65,3 +69,6 @@ if message_body:
     msg = MIMEText(message_body)
     msg['Subject'] = 'Harvesting log errors'
     _send_mail(mail_from, recipient_emails, msg)
+    harvester_check_log.write(message_body)
+
+harvester_check_log.close()
